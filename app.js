a9a8227a5ad25b4a1dd51715174712b57220f251
Pawel Dudek <paweldudek1988@gmail.com>
2018-04-26 17:02:11 +0900

    Modify JS file - hide dice image, connect random number with player score text

--- app.js
+++ app.js
@@ -16,3 +16,14 @@ roundScore = 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+//change CSS style!
+document.querySelector('.dice').style.display = 'none';
