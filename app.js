93dd678dcf23d03c408213ec8aed18c2226e9b67
Pawel Dudek <paweldudek1988@gmail.com>
2018-04-30 13:59:12 +0900

    Add function next player to change player in case of a) hold b) random number 1

--- app.js
+++ app.js
@@ -50,26 +50,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //roundScore = roundScore + dice (↑ the same result)
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        //next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        
-        // if(activePlayer ===0) {
-        //     activePlayer = 1;
-        // } else {
-        //     activePlayer = 0;
-        // }
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 }); 
@@ -85,8 +66,35 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     // Check IF player won the game
-})
+    
 
+    //Next Player
+    nextPlayer();
+
+});
+
+function nextPlayer() {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        
+    // if(activePlayer ===0) {
+    //     activePlayer = 1;
+    // } else {
+    //     activePlayer = 0;
+    // }
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 
